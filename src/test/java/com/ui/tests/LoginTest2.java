5daa480a04db77a9f91b94e28009963c3e5d2c52
LokiEYohan <lok.war74@gmail.com>
2026-01-19 01:10:12 +0530

    with enum static import in LoginTest2.java

--- src/test/java/com/ui/tests/LoginTest2.java
+++ src/test/java/com/ui/tests/LoginTest2.java
@@ -1,15 +1,14 @@
 package com.ui.tests;
 
-import com.constants.Browser;
 import com.ui.pages.HomePage;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+
+import static com.constants.Browser.CHROME;
 
 public class LoginTest2 {
     public static void main(String[] args) {
 //        WebDriver driver = new ChromeDriver();
 
-        HomePage homePage = new HomePage(Browser.CHROME);
+        HomePage homePage = new HomePage(CHROME);
 
         String loggedInAccountName = homePage.goToLoginPage().doLoginWith("tester01@mail.com", "Next@Gen@123").getLoggedInAccountName();
 
