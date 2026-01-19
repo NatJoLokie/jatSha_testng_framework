54dd79fd766b7614e097bab8d365cf1e41040d23
LokiEYohan <lok.war74@gmail.com>
2026-01-20 01:03:58 +0530

    Json file and Utility in Home Page to Launch URL

---
+++ src/test/java/com/utility/JsonTest.java
@@ -0,0 +1,22 @@
+package com.utility;
+
+import com.google.gson.Gson;
+import com.ui.pojo.Config;
+import com.ui.pojo.Environment;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class JsonTest {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        Gson gson = new Gson();
+        File jsonFile = new File(System.getProperty("user.dir") + "\\config\\config.json");
+        FileReader fileReader = new FileReader(jsonFile);
+        Config config = gson.fromJson(fileReader, Config.class);
+        Environment environment = config.getEnvironments().get("QA");
+        System.out.println(environment.getUrl());
+
+    }
+}
