b45557199d0eb49ee9eb95634f706695aaabda73
LokiEYohan <lok.war74@gmail.com>
2026-01-22 23:23:11 +0530

    Data Provider Utility, Json to Test Data

---
+++ src/test/java/com/ui/dataProviders/LoginDataProvider.java
@@ -0,0 +1,57 @@
+package com.ui.dataProviders;
+
+import com.google.gson.Gson;
+import com.ui.pojo.TestData;
+import com.ui.pojo.User;
+import org.testng.annotations.DataProvider;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class LoginDataProvider {
+
+    @DataProvider(name = "loginDataProvider")
+    public Iterator<Object[]> loginDataProvider() throws FileNotFoundException {
+
+        Gson gson = new Gson();
+        File testDataFile = new File(System.getProperty("user.dir") + "/testData/loginData.json");
+        System.out.println(testDataFile);
+
+        FileReader fileReader = new FileReader(testDataFile);
+        TestData testData = gson.fromJson(fileReader, TestData.class);
+
+        System.out.println(testData.toString());
+
+        List<Object[]> dataToReturn = new ArrayList<Object[]>();
+        for (User user : testData.getData()) {
+            Object[] wrapperArr = new Object[]{user};
+            dataToReturn.add(wrapperArr);
+        }
+/*
+//        List<Object[]> dataToReturn = testData.getData().stream()
+//                .map(user -> new Object[]{user}) // Wrap each user in an Object array
+//                .collect(Collectors.toList());
+*/
+
+/*
+//        @DataProvider(name = "userData") // TestNG example
+//        public Object[][] provideData() {
+//            return testData.getData().stream()
+//                    .map(user -> new Object[]{user})
+//                    .toArray(Object[][]::new); // Converting List to 2D Array is often preferred
+//        }
+*/
+
+
+        return dataToReturn.iterator();
+    }
+//        String emailAddress = testData.getData().get(0).getEmailAddress();
+//        String password = testData.getData().get(0).getPassword();
+//
+//        System.out.println(emailAddress + "," + password);
+//    }
+}
--- src/test/java/com/ui/tests/LoginTest3.java
+++ src/test/java/com/ui/tests/LoginTest3.java
@@ -1,6 +1,7 @@
 package com.ui.tests;
 
 import com.ui.pages.HomePage;
+import com.ui.pojo.User;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -16,13 +17,15 @@ public class LoginTest3 {
         homePage = new HomePage(CHROME);   // Setup code if needed
     }
 
-    @Test(description = "Verify if a valid user is able to login to the application", groups = {"regression", "sanity"})
-    public void LoginTest() {
+    @Test(description = "Verify if a valid user is able to login to the application",
+            groups = {"regression", "sanity"},
+            dataProviderClass = com.ui.dataProviders.LoginDataProvider.class, dataProvider = "loginDataProvider")
+    public void LoginTest(User user) {
 
 //        String loggedInAccountName = homePage.goToLoginPage().doLoginWith("tester01@mail.com", "Next@Gen@123").getLoggedInAccountName();
 //        Assert.assertEquals(loggedInAccountName, "Test User", "Logged in account name does not match expected value.");
 
-        Assert.assertEquals(homePage.goToLoginPage().doLoginWith("tester01@mail.com", "Next@Gen@123").getLoggedInAccountName(), "Next Gen Tester", "Logged in account name does not match expected value.");
+        Assert.assertEquals(homePage.goToLoginPage().doLoginWith(user.getEmailAddress(), user.getPassword()).getLoggedInAccountName(), "Next Gen Tester", "Logged in account name does not match expected value.");
 
     }
 
